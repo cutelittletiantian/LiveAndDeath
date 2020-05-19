b16752e031593b34c0acadb7b106bb3e452aaeaf
Kingfish404 <jinyu929@qq.com>
2020-05-19 20:32:24 +0800

    a

---
+++ Logic.java
@@ -0,0 +1,12 @@
+public class Logic {
+
+    // 更新map数据
+    public void update(Map map){
+        for (int i = 0; i < map.getLen(); i++) {
+            for (int j = 0; j < map.getLen(); j++) {
+
+                map.set(i,j,0);
+            }
+        }
+    }
+}
