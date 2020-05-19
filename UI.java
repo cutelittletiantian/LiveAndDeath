eeac0fa0410fa37b6c35a8b58e7aff9892b82fee
Kingfish404 <jinyu929@qq.com>
2020-05-19 21:05:35 +0800

    fix

--- UI.java
+++ UI.java
@@ -1,32 +1,69 @@
+import java.awt.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class UI {
-    Map map;
+    Map map = new Map();
+    String title = "生命游戏";
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JButton jButton[][];
 
     public void show(Map m) throws InterruptedException {
-        map=m;
-        clear();
-        int len = map.getLen();
-        for (int i = 0; i < len + 4; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+        // frame.setLayout(new GridLayout());
+        GridLayout gridLayout = new GridLayout();
+        jButton = new JButton[m.getLen()][m.getLen()];
 
-        for (int i = 0; i < len; i++) {
-            System.out.print("||");
-            for (int j = 0; j < len; j++) {
-                System.out.print(map.get(i, j));
+        panel.setLayout(gridLayout);
+        frame.add(panel);
+        for (int i = 0; i < m.getLen(); i++) {
+            for (int j = 0; j < m.getLen(); j++) {
+                jButton[i][j] = new JButton();
+                jButton[i][j].setSize(20,20);
             }
-            System.out.println("||");
         }
-        for (int i = 0; i < len + 4; i++) {
-            System.out.print("-");
+
+        for (int i = 0; i < m.getLen(); i++) {
+            for (int j = 0; j < m.getLen(); j++) {
+                panel.add(jButton[i][j]);
+                jButton[i][j].show();
+            }
         }
-        System.out.println();
-        Thread.sleep(500);
+
+        frame.setResizable(false);
+        frame.setTitle(title);
+        panel.show();
+        frame.show();
+
+        // map=m;
+        // clear();
+        // int len = map.getLen();
+        // for (int i = 0; i < len + 4; i++) {
+        // System.out.print("-");
+        // }
+        // System.out.println();
+
+        // for (int i = 0; i < len; i++) {
+        // System.out.print("||");
+        // for (int j = 0; j < len; j++) {
+        // System.out.print(map.get(i, j));
+        // }
+        // System.out.println("||");
+        // }
+        // for (int i = 0; i < len + 4; i++) {
+        // System.out.print("-");
+        // }
+        // System.out.println();
+        // Thread.sleep(500);
     }
 
     private void clear() {
         Map map = new Map();
-        for (int i = 0; i <map.getLen() ; i++) {
+        for (int i = 0; i < map.getLen(); i++) {
             System.out.println();
         }
     }
